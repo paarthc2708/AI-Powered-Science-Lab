1a063e1088957835e1e0bb59446bf921b3396a49
paarthc2708 <paarthchhattani2708@gmail.com>
2025-12-29 22:25:43 +0530

    Added multiple sections including transition screen, guided experiments section

--- sectionHiding.js
+++ sectionHiding.js
@@ -1,6 +1,19 @@
 let welcomePage = document.getElementById("welcomePage");
 let experimentsSection = document.getElementById("experimentsSection");
+let guidedExperimentsSection = document.getElementById("guidedExperimentsSection");
+let fromScratchSection = document.getElementById("fromScratchSection");
+
 function enterLab(){
     welcomePage.style.display = "none";
     experimentsSection.style.display = "block";
+}
+
+function startGuidedExperiments(){
+    experimentsSection.style.display = "none";
+    guidedExperimentsSection.style.display = "block";
+}
+
+function startFromScratch(){
+    experimentsSection.style.display = "none";
+    fromScratchSection.style.display = "block";
 }
